962fbaa7ac130a47894692909c1e57bd670ba31a
Lohany <158223098+Lohanyy17@users.noreply.github.com>
2025-05-30 15:06:05 -0300

    Add files via upload

---
+++ static/script/script.js
@@ -0,0 +1,19 @@
+function scrollParaSobre() {
+    document.getElementById("sobre").scrollIntoView({ behavior: 'smooth' });
+}
+ 
+function scrollParaHabilidades() {
+    document.getElementById("habilidades").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollParaSoft() {
+    document.getElementById("softskills").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollParaProjetos() {
+    document.getElementById("projetos").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollParaContatos() {
+    document.getElementById("contatos").scrollIntoView({ behavior: 'smooth' });
+}
